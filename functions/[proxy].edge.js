0a1e3070b1304c50f30fac4e6b266b9354ba19d0
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-12-17 15:26:03 +0530

    normalize // to /

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,9 +1,16 @@
 export default function handler(request, context) {
   const parsedUrl = new URL(request.url);
-  const route = parsedUrl.pathname;
+  const pathname = parsedUrl.pathname;
+
+  // Normalize consecutive slashes (//) to single slash (/)
+  if (pathname.includes('//')) {
+    const normalizedPath = pathname.replace(/\/{2,}/g, '/');
+    parsedUrl.pathname = normalizedPath;
+    return Response.redirect(parsedUrl, 308);
+  }
 
   // Redirect /contact to /about with 308 Permanent Redirect
-  if (route === '/contact') {
+  if (pathname === '/contact') {
     parsedUrl.pathname = '/about';
     return Response.redirect(parsedUrl, 308);
   }

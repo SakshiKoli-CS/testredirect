c296908196858ab638e63da02fe4565ec174fd4b
SakshiKoli-CS <sakshi.koli@contentstack.com>
2025-12-18 14:21:42 +0530

    test

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,4 +1,11 @@
 export default function handler(request, context) {
+  // Access environment variable from context (Cloudflare Workers style)
+  const environmentName = context?.env?.ENVIRONMENT_NAME;
+  
+  if (environmentName !== "test") {
+    return fetch(request);
+  }
+
   const parsedUrl = new URL(request.url);
   const pathname = parsedUrl.pathname;
 
